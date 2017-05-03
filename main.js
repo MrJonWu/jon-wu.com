6354eb15548a47b4698b2e418e77835163f3e49f
MrJonWu <mrjonwu@gmail.com>
2017-05-03 00:57:36 -0700

    Made the app more mobile friendly

--- main.js
+++ main.js
@@ -26,7 +26,47 @@ $(document).ready(function() {
   });
 
   window.sr = ScrollReveal();
+  sr.reveal('.sr-my-skills', {
+    duration: 600,
+    distance: '0px'
+  });
   sr.reveal('.sr-skills', {
+    duration: 600,
+    distance: '0px'
+  }, 200);
+  sr.reveal('.sr-experience', {
+    duration: 600,
+    distance: '0px'
+  });
+  sr.reveal('.sr-work', {
+    duration: 600,
+    distance: '0px'
+  }, 200);
+  sr.reveal('.sr-my-projects', {
+    duration: 600,
+    distance: '0px'
+  });
+  sr.reveal('.sr-narnia', {
+    duration: 600,
+    distance: '0px'
+  }, 200);
+  sr.reveal('.sr-moneyio', {
+    duration: 600,
+    distance: '0px'
+  }, 200);
+  sr.reveal('.sr-fridge', {
+    duration: 600,
+    distance: '0px'
+  }, 200);
+  sr.reveal('.sr-about', {
+    duration: 600,
+    distance: '0px'
+  });
+  sr.reveal('.sr-me', {
+    duration: 600,
+    distance: '0px'
+  }, 200);
+  sr.reveal('.sr-footer', {
     duration: 600,
     distance: '0px'
   }, 200);
